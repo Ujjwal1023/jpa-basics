3b82fd9a43555cedcc2055cc3f2f30b52f441828
UJJWAL'S_WORLD <kumarujjwal471@gmail.com>
2023-07-27 12:06:47 +0530

    jpa basics

---
+++ src/main/java/com/example/jpabasicsjuly/Student.java
@@ -0,0 +1,20 @@
+package com.example.jpabasicsjuly;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level= AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Student {
+    @Id
+    int rollNo;
+    String name;
+    int age;
+}
